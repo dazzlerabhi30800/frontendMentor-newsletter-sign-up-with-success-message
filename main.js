ffafa57d332a10d061b3a635813155b4153d6873
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-05-30 14:52:00 +0530

    added error handling

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const form = document.querySelector("form");
 const emailInput = document.querySelector("#email");
-var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 const front = document.querySelector(".front");
 const back = document.querySelector(".back");
 const dismissBtn = document.querySelector(".dismiss");
+const inputContainer = document.querySelector(".input--container");
+
+var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 form.onsubmit = (e) => {
   e.preventDefault();
   if (emailInput.value.match(mailformat)) {
+    inputContainer.classList.remove("error");
     front.classList.add("hide");
     back.classList.add("show");
+  } else {
+    inputContainer.classList.add("error");
   }
-  return;
 };
 
 dismissBtn.onclick = () => {
